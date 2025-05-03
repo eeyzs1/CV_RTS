b32657154546c78ee7dfbd900aaa2dbc15505525
eeyzs1.zoho.com <eeyzs1.zoho.com>
2025-05-03 22:41:36 +0800

    add rts and latex

--- optimizers.py
+++ optimizers.py
@@ -107,6 +107,251 @@ class PUGDX(torch.optim.Optimizer):
                )
         return norm
 
+# R for perturbation radius
+class PUGDXR(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, step_x = 0, min_beta = 0.1, max_beta = 3, method = 'sin', max_epochs = 40, **kwargs):
+        defaults = dict(**kwargs)
+        super(PUGDXR, self).__init__(params, defaults)
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+        self.wd = self.param_groups[0]['weight_decay']
+        self.mom = self.param_groups[0]['momentum']
+        self.step_x = step_x
+        self.max_epochs = max_epochs
+        self.min_beta = min_beta
+        self.max_beta = max_beta
+        self.method = method
+        self.update_alpha(0)
+
+    # sin: the alpha range from min to max, cos: range from max to min
+    def update_alpha(self, epoch):
+        match self.method:
+            case 'sin':
+                self.alpha = self.min_beta + (self.max_beta - self.min_beta) * sin(epoch/self.max_epochs * pi/2)
+            case 'cos':
+                self.alpha = self.min_beta + (self.max_beta - self.min_beta) * cos(epoch/self.max_epochs * pi/2)
+        
+    @torch.no_grad()
+    def step(self, closure=None):              
+        assert closure is not None, 'There should be a closure'
+        closure = torch.enable_grad()(closure)
+        self.first_step()
+        closure()
+        self.second_step(zero_grad=True)
+
+    @torch.no_grad()
+    def first_step(self):
+        abs_grad_norm = self._abs_grad_norm() / self.alpha
+        for group in self.param_groups:
+            for i, p in enumerate(group["params"]):
+                if p.grad is None: continue
+                self.state[i]["e_w"] = torch.abs(p) * p.grad / (abs_grad_norm + 1e-12)
+                p.add_(self.state[i]["e_w"])
+                # p.grad *= self.alpha
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            for i, p in enumerate(group["params"]):
+                if p.grad is None: continue
+                p.sub_(self.state[i]["e_w"])
+                p.grad = p.grad / (grad_norm + 1e-12)
+
+        self.base_optimizer.step()
+
+        if zero_grad: self.zero_grad()
+        
+        return grad_norm.cpu()
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  
+        norm = torch.norm(
+                    torch.stack([
+                        p.grad.norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                   p=2
+               )
+        return norm
+    
+    def _abs_grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  
+        norm = torch.norm(
+                    torch.stack([
+                        (torch.abs(p)*p.grad).norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                   p=2
+               )
+        return norm
+
+# T for timing
+class PUGDXT(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, step_x = 0, **kwargs):
+        defaults = dict(**kwargs)
+        super(PUGDXT, self).__init__(params, defaults)
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+        self.wd = self.param_groups[0]['weight_decay']
+        self.mom = self.param_groups[0]['momentum']
+        self.step_x = step_x
+
+        
+    @torch.no_grad()
+    def step(self, closure=None):              
+        assert closure is not None, 'There should be a closure'
+        closure = torch.enable_grad()(closure)
+        self.first_step()
+        closure()
+        self.second_step(zero_grad=True)
+        
+    @torch.no_grad()
+    def xp_step(self, zero_grad=True):  
+        '''UGD = NGD-FW in Tensor'''
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            for i, p in enumerate(group["params"]):
+                if p.grad is None: continue
+                p.grad = p.grad / (grad_norm + 1e-12)   
+        self.base_optimizer.step()
+        if zero_grad: self.zero_grad()
+    
+    @torch.no_grad()
+    def first_step(self):
+        abs_grad_norm = self._abs_grad_norm()
+        for group in self.param_groups:
+            for i, p in enumerate(group["params"]):
+                if p.grad is None: continue
+                self.state[i]["e_w"] = torch.abs(p) * p.grad/ (abs_grad_norm + 1e-12)
+                p.add_(self.state[i]["e_w"])
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            for i, p in enumerate(group["params"]):
+                if p.grad is None: continue
+                p.sub_(self.state[i]["e_w"])
+                p.grad = p.grad / (grad_norm + 1e-12)
+
+        self.base_optimizer.step()
+
+        if zero_grad: self.zero_grad()
+        
+        return grad_norm.cpu()
+
+    @torch.no_grad()
+    def base_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        self.base_optimizer.step()
+        if zero_grad: self.zero_grad()
+        return grad_norm.cpu()
+
+    @torch.no_grad()
+    def base_step_no_norm(self, zero_grad=False):
+        self.base_optimizer.step()
+        if zero_grad: self.zero_grad()
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  
+        norm = torch.norm(
+                    torch.stack([
+                        p.grad.norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                   p=2
+               )
+        return norm
+    
+    def _abs_grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  
+        norm = torch.norm(
+                    torch.stack([
+                        (torch.abs(p)*p.grad).norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                   p=2
+               )
+        return norm
+
+
+
+# S for scale of gradient
+class PUGDXS(torch.optim.Optimizer):
+    def __init__(self, params, base_optimizer, step_x = 0, min_beta = 0.1, max_beta = 3, method = 'sin', max_epochs = 40, **kwargs):
+        defaults = dict(**kwargs)
+        super(PUGDXS, self).__init__(params, defaults)
+        self.base_optimizer = base_optimizer(self.param_groups, **kwargs)
+        self.param_groups = self.base_optimizer.param_groups
+        self.wd = self.param_groups[0]['weight_decay']
+        self.mom = self.param_groups[0]['momentum']
+        self.step_x = step_x
+        self.max_epochs = max_epochs
+        self.min_beta = min_beta
+        self.max_beta = max_beta
+        self.method = method
+        self.update_alpha(0)
+
+    # sin: the alpha range from min to max, cos: range from max to min
+    def update_alpha(self, epoch):
+        match self.method:
+            case 'sin':
+                self.alpha = self.min_beta + (self.max_beta - self.min_beta) * sin(epoch/self.max_epochs * pi/2)
+            case 'cos':
+                self.alpha = self.min_beta + (self.max_beta - self.min_beta) * cos(epoch/self.max_epochs * pi/2)
+
+    @torch.no_grad()
+    def first_step(self):
+        abs_grad_norm = self._abs_grad_norm()
+        for group in self.param_groups:
+            for i, p in enumerate(group["params"]):
+                if p.grad is None: continue
+                self.state[i]["e_w"] = torch.abs(p) * p.grad / (abs_grad_norm + 1e-12)
+                p.add_(self.state[i]["e_w"])
+                p.grad /= self.alpha
+
+    @torch.no_grad()
+    def second_step(self, zero_grad=False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            for i, p in enumerate(group["params"]):
+                if p.grad is None: continue
+                p.sub_(self.state[i]["e_w"])
+                p.grad = p.grad / (grad_norm + 1e-12)
+        self.base_optimizer.step()
+        if zero_grad: self.zero_grad()
+        return grad_norm.cpu()
+
+    def _grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  
+        norm = torch.norm(
+                    torch.stack([
+                        p.grad.norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                   p=2
+               )
+        return norm
+    
+    def _abs_grad_norm(self):
+        shared_device = self.param_groups[0]["params"][0].device  
+        norm = torch.norm(
+                    torch.stack([
+                        (torch.abs(p)*p.grad).norm(p=2).to(shared_device)
+                        for group in self.param_groups for p in group["params"]
+                        if p.grad is not None
+                    ]),
+                   p=2
+               )
+        return norm
+
+
 
 class PUGDXA(torch.optim.Optimizer):
     def __init__(self, params, base_optimizer, step_x = 0, **kwargs):
@@ -356,6 +601,8 @@ class PUGDXSIN(torch.optim.Optimizer):
                    p=2
                )
         return norm
+
+
 
 class SAMX(torch.optim.Optimizer):
     def __init__(self, params, base_optimizer, step_x = 0, r_ho = 1, **kwargs):
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 import time, copy
+import numpy as np
 
 def train_model_org(model, criterion, optimizer, scheduler, num_epochs, trainloader, device, dataset_sizes):
     since = time.time()
@@ -66,6 +67,8 @@ def train_model_org(model, criterion, optimizer, scheduler, num_epochs, trainloa
                 best_acc = epoch_acc
                 # best_model_wts = copy.deepcopy(model.state_dict())
         # print()
+        if (epoch + 1)%10 == 0:
+            torch.cuda.empty_cache()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
@@ -148,6 +151,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs, trainloader,
                 best_acc = epoch_acc
                 # best_model_wts = copy.deepcopy(model.state_dict())
         # print()
+        if (epoch + 1)%10 == 0:
+            torch.cuda.empty_cache()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
@@ -158,6 +163,95 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs, trainloader,
     # model.load_state_dict(best_model_wts)
     return model, metrics
 
+
+
+def train_modelt(model, criterion, optimizer, scheduler, start_epochs, num_epochs, trainloader, device, dataset_sizes):
+    since = time.time()
+
+    # best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    metrics = {
+    'train': {
+        'loss': [],
+        'acc': [] },
+    'valid': {
+        'loss': [],
+        'acc': [] },
+    "bast_acc": 0        
+                 }
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        # print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+          
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in trainloader[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        if epoch > start_epochs:
+                            loss.backward()
+                            optimizer.first_step()
+                            
+                            outputs = model(inputs)        
+                            loss = criterion(outputs, labels)       
+                            loss.backward()
+                            optimizer.second_step(zero_grad=True)
+                        else:
+                            loss.backward()
+                            optimizer.step_org()
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            # print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            #     phase, epoch_loss, epoch_acc))
+            metrics[phase]['loss'].append(epoch_loss)
+            metrics[phase]['acc'].append(epoch_acc)
+            # deep copy the model
+            if phase == 'valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                # best_model_wts = copy.deepcopy(model.state_dict())
+        # print()
+        if (epoch + 1)%10 == 0:
+            torch.cuda.empty_cache()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    metrics['bast_acc'] = best_acc
+    # load best model weights
+    # model.load_state_dict(best_model_wts)
+    return model, metrics
 
 def train_model_alpha(model, criterion, optimizer, scheduler, num_epochs, trainloader, device, dataset_sizes):
     since = time.time()
@@ -232,6 +326,218 @@ def train_model_alpha(model, criterion, optimizer, scheduler, num_epochs, trainl
                 best_acc = epoch_acc
                 # best_model_wts = copy.deepcopy(model.state_dict())
         # print()
+        if (epoch + 1)%10 == 0:
+            torch.cuda.empty_cache()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    metrics['bast_acc'] = best_acc
+    # load best model weights
+    # model.load_state_dict(best_model_wts)
+    return model, metrics
+
+#init_t: when to calculate the init_grad_var, gamma: attenuation coefficient, k:sliding window length for how many epochs used for calculating var of grad norm 
+def train_model_timing_var(model, criterion, optimizer, scheduler, num_epochs, trainloader, device, dataset_sizes, init_t, gamma, k):
+    since = time.time()
+
+    # best_model_wts = copy.deepcopy(model.state_dict())
+    grad_norms =[]
+    best_acc = 0.0
+    enable_PUGD = False
+    metrics = {
+    'train': {
+        'loss': [],
+        'acc': [] },
+    'valid': {
+        'loss': [],
+        'acc': [] },
+    "bast_acc": 0,
+    "enable_pugd_epoch": -1,
+    "init_grad_var": -1,
+    "grad_var": -1
+                 }
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        # print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+          
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in trainloader[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        if enable_PUGD:
+                            optimizer.first_step()
+                            
+                            outputs = model(inputs)        
+                            loss = criterion(outputs, labels)       
+                            loss.backward()
+                            optimizer.second_step(zero_grad=True)
+                        else:
+                            grad_norms.append(optimizer.base_step(zero_grad=True))
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            # print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            #     phase, epoch_loss, epoch_acc))
+            metrics[phase]['loss'].append(epoch_loss)
+            metrics[phase]['acc'].append(epoch_acc)
+            # deep copy the model
+            if phase == 'valid' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                # best_model_wts = copy.deepcopy(model.state_dict())
+                # print()
+                if not enable_PUGD:
+                    if epoch == init_t:
+                        init_grad_var = np.var(grad_norms) 
+                    elif epoch > k and epoch > init_t:
+                        grad_var = np.var(grad_norms[-k:])
+                        if grad_var < gamma * init_grad_var: 
+                            enable_PUGD = True
+                            metrics['enable_pugd_epoch'] = epoch
+                            metrics['init_grad_var'] = init_grad_var
+                            metrics['grad_var'] = grad_var
+
+        if (epoch + 1)%10 == 0:
+            torch.cuda.empty_cache()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    metrics['bast_acc'] = best_acc
+    # load best model weights
+    # model.load_state_dict(best_model_wts)
+    return model, metrics
+
+
+#xi: num of epochs for cal delta base, mu: Proportional threshold, t:sampling period
+def train_model_timing_delta(model, criterion, optimizer, scheduler, num_epochs, trainloader, device, dataset_sizes, xi, mu, t):
+    since = time.time()
+
+    # best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+    delta_base = 0.0
+    delta = 0.0
+    enable_PUGD = False
+    metrics = {
+    'train': {
+        'loss': [],
+        'acc': [] },
+    'valid': {
+        'loss': [],
+        'acc': [] },
+    "bast_acc": 0,
+    "enable_pugd_epoch": -1,
+    "delta_base": -1,
+    "delta": -1
+                 }
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        # print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'valid']:
+          
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in trainloader[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        if enable_PUGD:
+                            optimizer.first_step()
+                            
+                            outputs = model(inputs)        
+                            loss = criterion(outputs, labels)       
+                            loss.backward()
+                            optimizer.second_step(zero_grad=True)
+                        else:
+                            optimizer.base_step_no_norm(zero_grad=True)
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            # print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            #     phase, epoch_loss, epoch_acc))
+            metrics[phase]['loss'].append(epoch_loss)
+            metrics[phase]['acc'].append(epoch_acc)
+            # deep copy the model
+            if phase == 'valid': 
+                if epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                # best_model_wts = copy.deepcopy(model.state_dict())
+                # print()
+                if not enable_PUGD:
+                    delta = np.abs(metrics['valid']['loss'][-1] - metrics['train']['loss'][-1])
+                    if epoch < xi:
+                        delta_base += delta
+                    elif epoch == xi:
+                        delta_base /= xi
+                    elif epoch % t == 0 and delta > mu * delta_base:
+                        enable_PUGD = True
+                        metrics['enable_pugd_epoch'] = epoch
+                        metrics['delta_base'] = delta_base
+                        metrics['delta'] = delta
+
+        if (epoch + 1)%10 == 0:
+            torch.cuda.empty_cache()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
@@ -320,6 +626,8 @@ def train_modelx(model, criterion, optimizer, scheduler, num_epochs, trainloader
                 best_acc = epoch_acc
                 # best_model_wts = copy.deepcopy(model.state_dict())
         # print()
+        if (epoch + 1)%10 == 0:
+            torch.cuda.empty_cache()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
@@ -407,6 +715,9 @@ def train_modelx_last(model, criterion, optimizer, scheduler, num_epochs, trainl
                 best_acc = epoch_acc
                 # best_model_wts = copy.deepcopy(model.state_dict())
         # print()
+        if (epoch + 1)%10 == 0:
+            torch.cuda.empty_cache()
+            
     metrics['bast_acc'] = best_acc
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
